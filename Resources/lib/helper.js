f54edb174eb10f53366fe394b614238440f9fd60
Marcel Pociot <marcelpociot@localhost.(none)>
2011-10-07 12:49:01 +0200

    init

---
+++ Resources/lib/helper.js
@@ -0,0 +1,341 @@
+/**
+ * All needed helper functions
+ * These functions where taken from phpjs.org
+ * 
+ */
+exports.timestamp	= function(){
+    return Math.floor(+new Date()/1000);
+};
+exports.combine		= function(/*Object*/ target, /*Object*/ source){
+	var name, s, i;
+	for(name in source){
+		s = source[name];
+		if(!(name in target) || (target[name] !== s && (!(name in empty) || empty[name] !== s))){
+			target[name] = s;
+		}
+	}
+	return target; // Object
+};
+/**
+ * sort object
+ */
+exports.sortObject	= function(o){
+    var sorted = {},
+    key, a = [];
+
+    for (key in o) {
+        if (o.hasOwnProperty(key)) {
+                a.push(key);
+        }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+        sorted[a[key]] = o[a[key]];
+    }
+    return sorted;
+};
+/**
+ * PHPs parse_str function
+ */
+exports.parse_str = function(str, array) {
+    var glue1 = '=',
+        glue2 = '&',
+        array2 = String(str).replace(/^&?([\s\S]*?)&?$/, '$1').split(glue2),
+        i, j, chr, tmp, key, value, bracket, keys, evalStr, that = this,
+        fixStr = function (str) {
+            return that.urldecode(str).replace(/([\\"'])/g, '\\$1').replace(/\n/g, '\\n').replace(/\r/g, '\\r');
+        };
+
+    if (!array) {
+        array = this.window;
+    }
+
+    for (i = 0; i < array2.length; i++) {
+        tmp = array2[i].split(glue1);
+        if (tmp.length < 2) {
+            tmp = [tmp, ''];
+        }
+        key = fixStr(tmp[0]);
+        value = fixStr(tmp[1]);
+        while (key.charAt(0) === ' ') {
+            key = key.substr(1);
+        }
+        if (key.indexOf('\0') !== -1) {
+            key = key.substr(0, key.indexOf('\0'));
+        }
+        if (key && key.charAt(0) !== '[') {
+            keys = [];
+            bracket = 0;
+            for (j = 0; j < key.length; j++) {
+                if (key.charAt(j) === '[' && !bracket) {
+                    bracket = j + 1;
+                } else if (key.charAt(j) === ']') {
+                    if (bracket) {
+                        if (!keys.length) {
+                            keys.push(key.substr(0, bracket - 1));
+                        }
+                        keys.push(key.substr(bracket, j - bracket));
+                        bracket = 0;
+                        if (key.charAt(j + 1) !== '[') {
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!keys.length) {
+                keys = [key];
+            }
+            for (j = 0; j < keys[0].length; j++) {
+                chr = keys[0].charAt(j);
+                if (chr === ' ' || chr === '.' || chr === '[') {
+                    keys[0] = keys[0].substr(0, j) + '_' + keys[0].substr(j + 1);
+                }
+                if (chr === '[') {
+                    break;
+                }
+            }
+            evalStr = 'array';
+            for (j = 0; j < keys.length; j++) {
+                key = keys[j];
+                if ((key !== '' && key !== ' ') || j === 0) {
+                    key = "'" + key + "'";
+                } else {
+                    key = eval(evalStr + '.push([]);') - 1;
+                }
+                evalStr += '[' + key + ']';
+                if (j !== keys.length - 1 && eval('typeof ' + evalStr) === 'undefined') {
+                    eval(evalStr + ' = [];');
+                }
+            }
+            evalStr += " = '" + value + "';\n";
+            eval(evalStr);
+        }
+    }
+};
+exports.percentEncode = function (s) {
+    if (s == null) {
+        return "";
+    }
+    s = encodeURIComponent(s);
+    // Now replace the values which encodeURIComponent doesn't do
+    // encodeURIComponent ignores: - _ . ! ~ * ' ( )
+    // OAuth dictates the only ones you can ignore are: - _ . ~
+    // Source: http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference:Global_Functions:encodeURIComponent
+    s = s.replace(/\!/g, "%21");
+    s = s.replace(/\*/g, "%2A");
+    s = s.replace(/\'/g, "%27");
+    s = s.replace(/\(/g, "%28");
+    s = s.replace(/\)/g, "%29");
+    return s;
+};
+exports.urldecode =function(str) {
+    return decodeURIComponent((str + '').replace(/\+/g, '%20'));
+};
+exports.urlencode = function(str) {
+    str = (str + '').toString();
+
+    // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+    // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+    return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+    replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
+}
+
+
+exports.sha1 = function (str) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+    // + namespaced by: Michael White (http://getsprink.com)
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // -    depends on: utf8_encode
+    // *     example 1: sha1('Kevin van Zonneveld');
+    // *     returns 1: '54916d2e62f65b3afa6e192e6a601cdbe5cb5897'
+    var rotate_left = function (n, s) {
+        var t4 = (n << s) | (n >>> (32 - s));
+        return t4;
+    };
+
+/*var lsb_hex = function (val) { // Not in use; needed?
+        var str="";
+        var i;
+        var vh;
+        var vl;
+
+        for ( i=0; i<=6; i+=2 ) {
+            vh = (val>>>(i*4+4))&0x0f;
+            vl = (val>>>(i*4))&0x0f;
+            str += vh.toString(16) + vl.toString(16);
+        }
+        return str;
+    };*/
+
+    var cvt_hex = function (val) {
+        var str = "";
+        var i;
+        var v;
+
+        for (i = 7; i >= 0; i--) {
+            v = (val >>> (i * 4)) & 0x0f;
+            str += v.toString(16);
+        }
+        return str;
+    };
+
+    var blockstart;
+    var i, j;
+    var W = new Array(80);
+    var H0 = 0x67452301;
+    var H1 = 0xEFCDAB89;
+    var H2 = 0x98BADCFE;
+    var H3 = 0x10325476;
+    var H4 = 0xC3D2E1F0;
+    var A, B, C, D, E;
+    var temp;
+
+    str = this.utf8_encode(str);
+    var str_len = str.length;
+
+    var word_array = [];
+    for (i = 0; i < str_len - 3; i += 4) {
+        j = str.charCodeAt(i) << 24 | str.charCodeAt(i + 1) << 16 | str.charCodeAt(i + 2) << 8 | str.charCodeAt(i + 3);
+        word_array.push(j);
+    }
+
+    switch (str_len % 4) {
+    case 0:
+        i = 0x080000000;
+        break;
+    case 1:
+        i = str.charCodeAt(str_len - 1) << 24 | 0x0800000;
+        break;
+    case 2:
+        i = str.charCodeAt(str_len - 2) << 24 | str.charCodeAt(str_len - 1) << 16 | 0x08000;
+        break;
+    case 3:
+        i = str.charCodeAt(str_len - 3) << 24 | str.charCodeAt(str_len - 2) << 16 | str.charCodeAt(str_len - 1) << 8 | 0x80;
+        break;
+    }
+
+    word_array.push(i);
+
+    while ((word_array.length % 16) != 14) {
+        word_array.push(0);
+    }
+
+    word_array.push(str_len >>> 29);
+    word_array.push((str_len << 3) & 0x0ffffffff);
+
+    for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+        for (i = 0; i < 16; i++) {
+            W[i] = word_array[blockstart + i];
+        }
+        for (i = 16; i <= 79; i++) {
+            W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+        }
+
+
+        A = H0;
+        B = H1;
+        C = H2;
+        D = H3;
+        E = H4;
+
+        for (i = 0; i <= 19; i++) {
+            temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+
+        for (i = 20; i <= 39; i++) {
+            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+
+        for (i = 40; i <= 59; i++) {
+            temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+
+        for (i = 60; i <= 79; i++) {
+            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+
+        H0 = (H0 + A) & 0x0ffffffff;
+        H1 = (H1 + B) & 0x0ffffffff;
+        H2 = (H2 + C) & 0x0ffffffff;
+        H3 = (H3 + D) & 0x0ffffffff;
+        H4 = (H4 + E) & 0x0ffffffff;
+    }
+
+    temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+    return temp.toLowerCase();
+};
+
+exports.utf8_encode = function (argString) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: sowberry
+    // +    tweaked by: Jack
+    // +   bugfixed by: Onno Marsman
+    // +   improved by: Yves Sucaet
+    // +   bugfixed by: Onno Marsman
+    // +   bugfixed by: Ulrich
+    // +   bugfixed by: Rafal Kukawski
+    // *     example 1: utf8_encode('Kevin van Zonneveld');
+    // *     returns 1: 'Kevin van Zonneveld'
+
+    if (argString === null || typeof argString === "undefined") {
+        return "";
+    }
+
+    var string = (argString + ''); // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+    var utftext = "",
+        start, end, stringl = 0;
+
+    start = end = 0;
+    stringl = string.length;
+    for (var n = 0; n < stringl; n++) {
+        var c1 = string.charCodeAt(n);
+        var enc = null;
+
+        if (c1 < 128) {
+            end++;
+        } else if (c1 > 127 && c1 < 2048) {
+            enc = String.fromCharCode((c1 >> 6) | 192) + String.fromCharCode((c1 & 63) | 128);
+        } else {
+            enc = String.fromCharCode((c1 >> 12) | 224) + String.fromCharCode(((c1 >> 6) & 63) | 128) + String.fromCharCode((c1 & 63) | 128);
+        }
+        if (enc !== null) {
+            if (end > start) {
+                utftext += string.slice(start, end);
+            }
+            utftext += enc;
+            start = end = n + 1;
+        }
+    }
+
+    if (end > start) {
+        utftext += string.slice(start, stringl);
+    }
+
+    return utftext;
+};
